133f49248306f450479c8f7b0154e3c6611dc878
beatricek222 <beatricek222@gmail.com>
2022-10-11 23:14:25 +0000

    got lat and long

--- umbrella.rb
+++ umbrella.rb
@@ -3,3 +3,19 @@ p "Hi! Where are you located?"
 user_location = gets.chomp
 
 p user_location
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U"
+
+require "open-uri"
+raw_data = URI.open(gmaps_url).read
+
+require "json"
+parsed_data = JSON.parse(raw_data)
+
+lat_long_hash = parsed_data["results"][0]["geometry"]["location"]
+
+latitude = lat_long_hash["lat"]
+longitude = lat_long_hash["lng"]
+
+p latitude
+p longitude
