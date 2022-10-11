1bd0790e997c8b35d73b041f8ab8e65f50150513
beatricek222 <beatricek222@gmail.com>
2022-10-11 23:23:47 +0000

    Current Weather

--- umbrella.rb
+++ umbrella.rb
@@ -4,7 +4,9 @@ user_location = gets.chomp
 
 p user_location
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U"
+gmaps_token = ENV.fetch("GMAPS_TOKEN")
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_token}"
 
 require "open-uri"
 raw_data = URI.open(gmaps_url).read
@@ -19,3 +21,17 @@ longitude = lat_long_hash["lng"]
 
 p latitude
 p longitude
+
+dark_sky_token = ENV.fetch("DARK_SKY_TOKEN")
+
+dark_sky_url = "https://api.darksky.net/forecast/#{dark_sky_token}/#{latitude},#{longitude}"
+
+data_sky = URI.open(dark_sky_url).read
+parsed_sky_data = JSON.parse(data_sky)
+
+weather_now = parsed_sky_data["currently"]
+
+temp_now = weather_now["temperature"]
+summary_now = weather_now["summary"]
+
+p "Currently, the temperature is #{temp_now} and there is #{summary_now}"
