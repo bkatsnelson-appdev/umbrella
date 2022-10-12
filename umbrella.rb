8aa94dcb47a9b27f120cca024557d8b33c20bb1e
beatricek222 <beatricek222@gmail.com>
2022-10-12 00:20:30 +0000

    Sends correct text message

--- umbrella.rb
+++ umbrella.rb
@@ -52,8 +52,35 @@ rain = 0
   end
 end
 
+
+# if rain > 0
+#   p "You might want to carry an umbrella!"
+# else
+#   p "You probably won't need an umbrella today."
+# end
+
+require "twilio-ruby"
+
+# Get your credentials from your Twilio dashboard, or from Canvas if you're using mine
+twilio_sid = "AC28922e0822d827ee29834fe1dc6f681e"
+twilio_token = "b7cfcdffa1800ad47260ec5c04da6975"
+twilio_sending_number = "+13126636198"
+
+# Create an instance of the Twilio Client and authenticate with your API key
+twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+
+# Craft your SMS as a Hash literal with three keys
 if rain > 0
-  p "You might want to carry an umbrella!"
+  body = "It's going to rain today - take an umbrella!"
 else
-  p "You probably won't need an umbrella today."
+  body = "You probably won't need an umbrella today!"
 end
+
+sms_info = {
+  :from => twilio_sending_number,
+  :to => "+18478679419", # Put your own phone number here if you want to see it in action
+  :body => body
+}
+
+# Send your SMS!
+twilio_client.api.account.messages.create(sms_info)
